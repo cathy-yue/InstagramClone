c3dd2c6e1517447d304f1796afcfbf49e110ea3b
Cathy Yue <cy18@my.fsu.edu>
2020-11-22 11:48:23 -0500

    Tried to add sign up

--- app/src/main/java/com/example/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -21,6 +21,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        btnSignUp = findViewById(R.id.btnSignUp);
         btnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -41,6 +43,13 @@ public class LoginActivity extends AppCompatActivity {
                 String username = etUsername.getText().toString();
                 String password = etPassword.getText().toString();
                 loginUser(username, password);
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goSignUpActivity();
             }
         });
     }
@@ -66,6 +75,12 @@ public class LoginActivity extends AppCompatActivity {
 
     private void goMainActivity(){
         Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    private void goSignUpActivity(){
+        Intent i = new Intent(this, SignUpActivity.class);
         startActivity(i);
         finish();
     }
